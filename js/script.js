09137ba3c51d18cb5754761b491e322058ebe28f
feLycks <felix.obiero@student.moringaschool.com>
2021-11-23 09:08:02 +0300

    script builb

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+$(document).ready(() => {
+    $('#design').click(() => {
+        $('.p-1').show();
+    })
+})
+
 function getData() {
-    // get values from user 
-    // check for empty fields 
-    // jQuery hover effect && displays 
-    // push to postgress 
+    const userName = document.getElementById('name').value;
+    const userEmail = document.getElementById('email').value;
+    const txtMessage = document.getElementById('message').value;
+
+    if (userName == '') {
+        alert('fill all fields')
+    } else if (userEmail == '') {
+        alert('empty email fields')
+    } else if (txtMessage == '') {
+        alert('empy message')
+    } else {
+        alert(`${userName} we have received your message. Thank you for reaching out to us!`);
+    }
+
 }
